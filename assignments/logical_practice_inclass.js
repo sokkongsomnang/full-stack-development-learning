aa3ed7e41eec6ec08368313f26fce5bcff7509e6
Sokkong Somnang <sokkongsomnang@gmail.com>
2026-03-26 22:59:22 +0700

    chore(exercise): logical practice class exercise

---
+++ assignments/logical_practice_inclass.js
@@ -0,0 +1,13 @@
+const username = "a"
+const password = "a1234"
+let inputUsername = "b"
+let inputPassword = "a1234"
+let isAdmin = true
+if ((username == inputUsername && password == inputPassword) || isAdmin) {
+    console.log("Login Successfully!!!")
+} else {
+    console.log("Login Failed!!!!")
+}
+for(let i = 0; i <= 9; i++) {
+    console.log(i)
+}
